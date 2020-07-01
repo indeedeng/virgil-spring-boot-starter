2d0321df29bbbe425a0ae0b9b21961838be72755
Richard Reedy <rreedy@indeed.com>
2020-07-01 12:36:48 -0500

    Checking in built artifacts for Prod; Upped version to version = '1.1.0-SNAPSHOT'

---
+++ scripts/tagForRelease.sh
@@ -0,0 +1,85 @@
+#!/bin/bash
+
+#####
+# tagForRelease.sh
+#
+# Our publish server cannot handle running the npm/frontend steps required to build the artifacts, so this script
+# will
+# 1) build the frontend (buildFrontEndProd)
+# 2) Increment the version in gradle (to whatever was passed in)
+# 3) Commit to master
+# 4) Create tag using version
+#####
+
+#Assign flags to variables
+while getopts v: flag
+do
+    case "${flag}" in
+        v) NEW_VERSION=${OPTARG};;
+    esac
+done
+
+#Verify given version is valid
+if [[ "$NEW_VERSION" == "" ]]; then
+  echo "Version (-v) flag must have a value in format #.#.#"
+  exit 1;
+fi
+
+#Verify new version is #.#.# format
+if [[ $(echo "$NEW_VERSION" | grep -oE "[[:digit:]]+\.[[:digit:]]+\.[[:digit:]]+") == "" ]]; then
+  echo "Version (-v) flag is not a valid format.  Expected format #.#.#. [Received: ${NEW_VERSION}]"
+  exit 1;
+fi
+
+
+# Verify that branch is set to master
+#CURRENT_BRANCH=$(git rev-parse --abbrev-ref HEAD)
+#
+## fail if not set
+#if [[ "$CURRENT_BRANCH" != "master" ]]; then
+#  echo "Change to master branch before running this script. [Current Branch: $CURRENT_BRANCH]"
+#  exit 1
+#fi
+
+## Debug logs
+#echo "$CURRENT_BRANCH"
+
+
+
+# build npm resources
+cd ..
+./gradlew buildFrontEndProd
+cd scripts
+
+if [ $? -ne 0 ]; then
+  echo "Gradle buildFrontEndProd did not complete successfully, not moving forward."
+  exit 2;
+fi
+
+
+# parse version
+BUILD_GRADLE_PATH="../build.gradle"
+
+# Find current version and only return matched string
+VERSION_STRING=$(grep -oE "version.*=.*'[[:digit:]]+.[[:digit:]]+.[[:digit:]]+-SNAPSHOT'" $BUILD_GRADLE_PATH)
+
+# parse current version
+VERSION_PREFIX=$(echo "$VERSION_STRING" | cut -f1 -d= | tr -d '[:space:]')
+CURRENT_VERSION=$(echo "$VERSION_STRING" | cut -f2 -d= | tr -d '[:space:]')
+echo "Parsed existing version. Replacing with new version. [Current Version: ${CURRENT_VERSION}] [New Version: ${NEW_VERSION}]"
+
+## Debug logs
+#echo $VERSION_STRING
+#echo $CURRENT_VERSION
+#echo "${VERSION_PREFIX} = ${CURRENT_VERSION}"
+
+NEW_VERSION_STRING="${VERSION_PREFIX} = '${NEW_VERSION}-SNAPSHOT'"
+
+#replace version
+sed -i "s/${VERSION_STRING}/${NEW_VERSION_STRING}/i" $BUILD_GRADLE_PATH
+
+
+
+# commit
+git commit -am "Checking in built artifacts for Prod; Upped version to ${NEW_VERSION_STRING}"
+
